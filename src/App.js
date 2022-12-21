f7621ffcb3d6f5ea1229e0da035d8e0da269f34a
kimtanook <seob6402@gmail.com>
2022-12-21 21:22:57 +0900

    Edit, basic setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import Home from "./components/pages/Home";
+import {useDispatch} from "react-redux";
+import {getTodos} from "./redux/modules/todoModule";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTodos());
+  }, []);
+
+  return <Home />;
 }
 
 export default App;
